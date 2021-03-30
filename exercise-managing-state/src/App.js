875ef3001b2863109433dfc1ee3d2d244ca6254e
TimLiu2019 <qitingliu7@gmail.com>
2021-03-30 14:40:25 -0400

    managing state: another solutiion

--- exercise-managing-state/src/App.js
+++ exercise-managing-state/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Score from './Score';
+import Game from './Game';
 
 // const value1 = Math.floor(Math.random() * 100);
 // const value2 = Math.floor(Math.random() * 100);
@@ -10,7 +12,7 @@ import './App.css';
 // const numCorrect = 0;
 
 class App extends Component {
-  constructor(props) {
+ /* constructor(props) {
     // Required step: always call the parent class' constructor
     super(props);
 
@@ -113,7 +115,43 @@ class App extends Component {
       </div>
     );
   }
-}
+} */
 
+
+
+ state = {
+  correctAnswer: 0,
+  numQuestions: 0,
+};
+
+
+
+handleAnswer = answerWasCorrect => {
+  if (answerWasCorrect) {
+    this.setState(currState => ({
+      correctAnswer: currState.correctAnswer + 1,
+    }));
+  }
+  this.setState(currState => ({
+    numQuestions: currState.numQuestions + 1,
+  }));
+};
+
+render() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">ReactND - Coding Practice</h1>
+      </header>
+      <div className="game">
+        <h2>Mental Math</h2>
+        <Game handleAnswer={this.handleAnswer} />
+        <Score numCorrect={this.state.correctAnswer} numQuestions={this.state.numQuestions} />
+      </div>
+    </div>
+  );
+}
+}
 export default App;
 
