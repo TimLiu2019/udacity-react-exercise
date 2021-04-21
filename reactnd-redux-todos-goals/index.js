5d5ccf4bdf68f1ac42c2c70bb262d07ef7bb5f6b
TimLiu2019 <qitingliu7@gmail.com>
2021-04-21 07:06:59 -0400

    use fuction app as root reducer

--- reactnd-redux-todos-goals/index.js
+++ reactnd-redux-todos-goals/index.js
@@ -54,37 +54,78 @@ function goals(state = [], action) {
     case "ADD_GOAL":
       return state.concat([action.goal]);
     case "REMOVE_GOAL":
-      return state.filter(goal => goal.id != action.id);
+      return state.filter(goal => goal.id !== action.id);
     default:
       return state;
   }
 }
+//
+function app(state = {}, action) {
+  return {
+    todos: todos(state.todos, action),
+    goals: goals(state.goals, action)
+  };
+}
+const store = createStore(app);
 
-const store1 = createStore(todos);
-const store2 = createStore(goals);
-
-store1.subscribe(() => {
-  console.log("The new state is: ", store1.getState());
+store.subscribe(() => {
+  console.log("The new state is: ", store.getState());
 });
 
-store2.subscribe(() => {
-    console.log("The new state is: ", store2.getState());
-  });
-
-store1.dispatch({
+store.dispatch({
   type: "ADD_TODO",
   todo: {
     id: 0,
-    name: "Learn Redux",
+    name: "Walk the dog",
     complete: false
   }
 });
 
-store2.dispatch({
-    type: "ADD_GOAL",
-    goal: {
-      id: 0,
-      name: "Learn front end",
-      complete: false
-    }
-  });
+store.dispatch({
+  type: "ADD_TODO",
+  todo: {
+    id: 1,
+    name: "Wash the car",
+    complete: false
+  }
+});
+
+store.dispatch({
+  type: "ADD_TODO",
+  todo: {
+    id: 2,
+    name: "Go to the gym",
+    complete: true
+  }
+});
+
+store.dispatch({
+  type: "REMOVE_TODO",
+  id: 1
+});
+
+store.dispatch({
+  type: "TOGGLE_TODO",
+  id: 0
+});
+
+store.dispatch({
+  type: "ADD_GOAL",
+  goal: {
+    id: 0,
+    name: "Learn Redux"
+  }
+});
+
+store.dispatch({
+  type: "ADD_GOAL",
+  goal: {
+    id: 1,
+    name: "Lose 20 pounds"
+  }
+});
+
+store.dispatch({
+  type: "REMOVE_GOAL",
+  id: 0
+});
